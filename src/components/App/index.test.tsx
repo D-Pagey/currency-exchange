de9e398221046b01d988d8d6a949bb498adaf0fd
Dan Page <dan.page91@gmail.com>
2020-05-08 12:25:51 +0100

    chore(linting): add eslint, prettier and husky

--- src/components/App/index.test.tsx
+++ src/components/App/index.test.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import App from ".";
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from '.';
 
-describe("App component", () => {
-  it("should render", () => {
-    const { container } = render(<App />);
-    expect(container.firstChild).toMatchSnapshot();
-  });
+describe('App component', () => {
+    it('should render', () => {
+        const { container } = render(<App />);
+        expect(container.firstChild).toMatchSnapshot();
+    });
 });
