b46a1e5b3b397aff46caf73dd629d0f99dd27470
Dan Page <dan.page91@gmail.com>
2020-05-14 21:07:09 +0100

    refactor(rates): move api call into relevant page

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,10 +1,7 @@
-import React, { FC, useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { FC, useState } from 'react';
 import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
-import { fromUnixTime } from 'date-fns';
 
-import { AccountsType, RatesType } from '../../types';
-// import { useInterval } from '../../hooks/useInterval';
+import { AccountsType } from '../../types';
 import { PageHome } from '../PageHome';
 import { PageExchange } from '../PageExchange';
 import { Header } from '../Header';
@@ -18,26 +15,7 @@ const initialAccounts = {
 };
 
 const App: FC = () => {
-    const [rates, setRates] = useState<RatesType>();
-    const [updatedDate, setUpdatedDate] = useState<Date>();
     const [accounts, setAccounts] = useState<AccountsType>(initialAccounts);
-
-    const fetchData = async () => {
-        const { data } = await axios.get(
-            `https://openexchangerates.org/api/latest.json?app_id=${process.env.REACT_APP_API_ID}`,
-        );
-
-        setRates({ USD: 1, GBP: data.rates.GBP, EUR: data.rates.EUR });
-        setUpdatedDate(fromUnixTime(data.timestamp));
-    };
-
-    useEffect(() => {
-        fetchData();
-    }, []);
-
-    // useInterval(() => {
-    //     fetchData();
-    // }, 10000);
 
     return (
         <BrowserRouter>
@@ -48,20 +26,12 @@ const App: FC = () => {
 
                 <Switch>
                     <Route exact path="/" component={(props: any) => <PageHome {...props} accounts={accounts} />} />
-                    {rates && updatedDate && (
-                        <Route
-                            path="/exchange"
-                            component={(props: any) => (
-                                <PageExchange
-                                    {...props}
-                                    accounts={accounts}
-                                    rates={rates}
-                                    setAccounts={setAccounts}
-                                    updatedDate={updatedDate}
-                                />
-                            )}
-                        />
-                    )}
+                    <Route
+                        path="/exchange"
+                        component={(props: any) => (
+                            <PageExchange {...props} accounts={accounts} setAccounts={setAccounts} />
+                        )}
+                    />
                 </Switch>
 
                 <S.ButtonWrapper>
--- src/components/PageExchange/index.tsx
+++ src/components/PageExchange/index.tsx
@@ -1,6 +1,9 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
+import axios from 'axios';
 import Select from 'react-select';
-import { format } from 'date-fns';
+import { format, fromUnixTime } from 'date-fns';
+
+import { useInterval } from '../../hooks/useInterval';
 import { EUR, GBP, USD, currencies } from '../../constants';
 import { AccountsType, RatesType } from '../../types';
 import { getValueFromRates } from '../../utils';
@@ -9,9 +12,7 @@ import * as S from './styles';
 
 export type PageExchangeTypes = {
     accounts: AccountsType;
-    rates: RatesType;
     setAccounts: (accounts: AccountsType) => void;
-    updatedDate: Date;
 };
 
 type Currencies = 'USD' | 'EUR' | 'GBP';
@@ -31,7 +32,9 @@ const dropdownOptions = [
     },
 ];
 
-export const PageExchange: FC<PageExchangeTypes> = ({ accounts, rates, setAccounts, updatedDate }) => {
+export const PageExchange: FC<PageExchangeTypes> = ({ accounts, setAccounts }) => {
+    const [rates, setRates] = useState<RatesType>();
+    const [updatedDate, setUpdatedDate] = useState<Date>();
     const [exchangeFromValue, setExchangeFromValue] = useState<number>(0);
     const [exchangeToValue, setExchangeToValue] = useState<number>(0);
     const [currencyFrom, setCurrencyFrom] = useState<Currencies>(USD);
@@ -39,6 +42,23 @@ export const PageExchange: FC<PageExchangeTypes> = ({ accounts, rates, setAccoun
 
     const valueTooLarge = exchangeFromValue > accounts[currencyFrom];
     const invalidFromValue = exchangeFromValue <= 0 || valueTooLarge;
+
+    const fetchData = async () => {
+        const { data } = await axios.get(
+            `https://openexchangerates.org/api/latest.json?app_id=${process.env.REACT_APP_API_ID}`,
+        );
+
+        setRates({ USD: 1, GBP: data.rates.GBP, EUR: data.rates.EUR });
+        setUpdatedDate(fromUnixTime(data.timestamp));
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    useInterval(() => {
+        fetchData();
+    }, 10000);
 
     const handleDropdownChange = (fromOrTo: string) => (option: any) => {
         if (fromOrTo === 'from') setCurrencyFrom(option.value);
@@ -88,58 +108,62 @@ export const PageExchange: FC<PageExchangeTypes> = ({ accounts, rates, setAccoun
         setAccounts(updatedAccounts);
     };
 
-    return (
-        <S.Wrapper>
-            <S.Title>Exchange Currencies:</S.Title>
+    if (rates && updatedDate) {
+        return (
+            <S.Wrapper>
+                <S.Title>Exchange Currencies:</S.Title>
 
-            <S.ItalicSmall>(Rates last updated at {format(updatedDate, 'PPpp')})</S.ItalicSmall>
+                <S.ItalicSmall>(Rates last updated at {format(updatedDate, 'PPpp')})</S.ItalicSmall>
 
-            <S.Text>
-                Current rate: {currencies[currencyFrom]}1 = {currencies[currencyTo]}
-                {getValueFromRates(currencyFrom, currencyTo, rates, 1)}
-            </S.Text>
+                <S.Text>
+                    Current rate: {currencies[currencyFrom]}1 = {currencies[currencyTo]}
+                    {getValueFromRates(currencyFrom, currencyTo, rates, 1)}
+                </S.Text>
 
-            <form>
-                <S.Grid>
-                    <Select
-                        options={dropdownOptions}
-                        onChange={handleDropdownChange('from')}
-                        value={{ label: currencyFrom, value: currencyFrom }}
-                    />
+                <form>
+                    <S.Grid>
+                        <Select
+                            options={dropdownOptions}
+                            onChange={handleDropdownChange('from')}
+                            value={{ label: currencyFrom, value: currencyFrom }}
+                        />
 
-                    {exchangeFromValue > 0 && <S.Operator>-</S.Operator>}
+                        {exchangeFromValue > 0 && <S.Operator>-</S.Operator>}
 
-                    <S.Input value={exchangeFromValue} type="number" onChange={handleExchangeFromChange} />
+                        <S.Input value={exchangeFromValue} type="number" onChange={handleExchangeFromChange} />
 
-                    <S.GridText invalid={valueTooLarge}>
-                        Balance: {currencies[currencyFrom]}
-                        {accounts[currencyFrom]}
-                    </S.GridText>
+                        <S.GridText invalid={valueTooLarge}>
+                            Balance: {currencies[currencyFrom]}
+                            {accounts[currencyFrom]}
+                        </S.GridText>
 
-                    <Select
-                        options={dropdownOptions}
-                        onChange={handleDropdownChange('to')}
-                        value={{ label: currencyTo, value: currencyTo }}
-                    />
+                        <Select
+                            options={dropdownOptions}
+                            onChange={handleDropdownChange('to')}
+                            value={{ label: currencyTo, value: currencyTo }}
+                        />
 
-                    {exchangeToValue > 0 && <S.Operator>+</S.Operator>}
+                        {exchangeToValue > 0 && <S.Operator>+</S.Operator>}
 
-                    <S.Input value={exchangeToValue} type="number" onChange={handleExchangeToChange} />
+                        <S.Input value={exchangeToValue} type="number" onChange={handleExchangeToChange} />
 
-                    <S.GridText>
-                        Balance: {currencies[currencyTo]}
-                        {accounts[currencyTo]}
-                    </S.GridText>
-                </S.Grid>
+                        <S.GridText>
+                            Balance: {currencies[currencyTo]}
+                            {accounts[currencyTo]}
+                        </S.GridText>
+                    </S.Grid>
 
-                <S.ButtonWrapper>
-                    <Button onClick={handleSwap}>Swap</Button>
+                    <S.ButtonWrapper>
+                        <Button onClick={handleSwap}>Swap</Button>
 
-                    <Button onClick={handleExchange} isDisabled={invalidFromValue}>
-                        Exchange
-                    </Button>
-                </S.ButtonWrapper>
-            </form>
-        </S.Wrapper>
-    );
+                        <Button onClick={handleExchange} isDisabled={invalidFromValue}>
+                            Exchange
+                        </Button>
+                    </S.ButtonWrapper>
+                </form>
+            </S.Wrapper>
+        );
+    }
+
+    return <div />;
 };
